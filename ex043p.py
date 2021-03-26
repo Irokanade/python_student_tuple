4d8723d17c39d2c9257ea61f54a68e86ac207cd1
Irokanade <67633095+Irokanade@users.noreply.github.com>
2021-03-26 23:26:43 +0800

    average mark added

--- ex043p.py
+++ ex043p.py
@@ -1,14 +1,31 @@
 i = 1
 studentList = []
+chineseTotal = 0
+englishTotal = 0
+mathTotal = 0
+chineseAvg = 0
+englishAvg = 0
+mathAvg = 0
 
 while i < 4:
     temp = str(input('enter student {0} name : '.format(i)))
     studentList.append(temp)    
 
     temp = str(input("enter student {0} id: ".format(i)))
-    while len(temp) != 5:
-        print("id must be 5 characters long, try again")
+
+    validNum = 0
+    for j in temp:
+        #print(i)
+        if j.isdigit():
+            validNum += 1
+
+    while validNum != 5:
+        validNum = 0
+        print("id must be 5 characters long and all digit characters, try again")
         temp = str(input("enter student {0} id: ".format(i)))
+        for j in temp:
+            if j.isdigit():
+                validNum += 1
 
     studentList.append(temp)
     temp = float(input("enter student {0} chinese score: ".format(i)))
@@ -34,5 +51,54 @@ while i < 4:
 
 tuple(studentList)
 
-for i in studentList:
-    print(i)
+'''
+for j in range(1, 3):
+    print(studentList[j])
+
+'''
+
+#chinese total
+#print('chinese')
+j = 2
+while j <= len(studentList):
+    #print(studentList[j])
+    chineseTotal += studentList[j]
+    j += 5
+    
+#english total
+#print('english')
+j = 3
+while j <= len(studentList):
+    #print(studentList[j])
+    englishTotal += studentList[j]
+    j += 5
+
+#maths total
+j = 4
+while j <= len(studentList):
+    #print(studentList[j])
+    mathTotal += studentList[j]
+    j += 5
+    
+
+#print('studnetlist/5: {0}'.format(str(len(studentList)/5)))
+chineseAvg = chineseTotal/(len(studentList)/5)
+englishAvg = englishTotal/(len(studentList)/5)
+mathAvg = mathTotal/(len(studentList)/5)
+
+'''
+print(chineseTotal)
+print(englishTotal)
+print(mathTotal)
+'''
+
+print(chineseAvg)
+print(englishAvg)
+print(mathAvg)
+
+
+
+
+
+
+ 
